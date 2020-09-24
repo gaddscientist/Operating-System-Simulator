ef8fd6f88bc4c3f01ec8c7029d83761debbc2a2b
Bryan Hilldrup <hilldrupbf@vcu.edu>
2020-09-24 13:10:42 -0400

    Implemented state switching function and process creation from cmd line argument

--- process.c
+++ process.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <time.h>
 
 #define MAXREGISTERS 10
 #define MAXINSTRUCTIONS 1000
 
 int next_pid = 0;
+int totalProcesses = 0;
 typedef enum {NEW, RUNNING, WAITING, READY, TERMINATED} state;
+state initialState = NEW;
 
 struct PCB {
     // Unique identifier of process
@@ -49,18 +53,40 @@ struct PCB* PCB_new(int pid, state currentState, int progCount, int reqMem, int 
 
 struct Process* Process_new() {
     struct Process* p = malloc(sizeof(struct Process));
-    state initialState = NEW;
     p->pid = next_pid++;
     p->pcb = *PCB_new(p->pid, initialState, 0, 1000, 0, 75);
+    totalProcesses++;
     return p;
 }
 
+void UpdateState(struct PCB* pcb, state newState) {
+    pcb->currentState = newState;
+}
 
 // process destructor
 // free(p)
 
 
 
-int main() {
+// CLEAN UP THIS MESS
+int main(int argc, char *argv[]) {
+    if (argc <= 2) {
+        printf("Please specify a template and number of processes to be created\n");
+        printf("Syntax: ./process template.txt number_of_processes\n");
+        printf("Example: ./process web_browser.txt 7\n");
+        return 1;
+    }
+
+    char* template = argv[1];
+    int numProcesses =  *argv[2] - '0';
+    struct Process* processes[2000];
+    int index;
+
+    printf("Number of processes is %d\n", numProcesses);
+    for (index = 0; numProcesses > 0; numProcesses--, index++) {
+        processes[index] = Process_new();
+        printf("Process: %d\n", processes[index]->pid);
+    }
+
     return 0;
 }
